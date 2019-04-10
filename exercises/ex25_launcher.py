0b07a56772a6ba81d817861f0dbcbcc35a90c02c
ocxs <ocxs@ocxsdb.ocxs.com>
2019-04-09 21:04:59 -0500

    till ex31

---
+++ exercises/ex25_launcher.py
@@ -0,0 +1,7 @@
+import ex25
+sentence = "All good things come to those who wait"
+
+#ex25.print_first_and_last(sentence)
+#ex25.print_first_and_last_sorted(sentence)
+help(ex25.break_words)
+help(ex25)
